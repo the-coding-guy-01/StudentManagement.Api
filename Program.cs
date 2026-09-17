917f44232b266cdd4d8ca52654ec4011cef769a8
the-coding-gut-01 <thecodingguy01@gmail.com>
2026-09-17 12:54:01 +0530

    Registered the application DB context service

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
