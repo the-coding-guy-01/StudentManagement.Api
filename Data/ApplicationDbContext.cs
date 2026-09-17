10ee04033f47672d4a2812be2b8914ebd551362f
the-coding-gut-01 <thecodingguy01@gmail.com>
2026-09-17 12:53:25 +0530

    Created the application DB Context file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Api.Models;
+
+namespace StudentManagement.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
